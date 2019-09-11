5cc719f802e8df813e5be8f21169a4ffc43e3398
Alex <aa.builov@gmail.com>
2019-09-11 13:29:34 +0300

    v.0.4

--- app/src/main/java/com/builov/mathgames/MainActivity.java
+++ app/src/main/java/com/builov/mathgames/MainActivity.java
@@ -1,6 +1,8 @@
 package com.builov.mathgames;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.Snackbar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,26 +20,60 @@ public class MainActivity extends AppCompatActivity {
     TextView tvSecondMathNumber;
     TextView tvMathSign;
     TextView tvAnswer;
+    TextView tvHintCount;
+    TextView tvVersion;
     EditText mEditTextAnswer;
     Button mButtonOk;
     Button mButtonHelp;
     Button mButtonDelete;
     Button[] btn = new Button[10];
-    
+
     private int firstNumber = 0;
     private int secondNumber = 0;
     private int intMathSign = 0;
     private int tempAnswer = 0;
     private int difficulty;
+    private int hintCount;
+
+    Thread secondThread;
+    Handler mHandler = new Handler();
+
+
     Boolean reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        double VERSION = 0.4;
+
         reset = true;
         initUI();
+
+
+        tvVersion.setText("v." + Double.toString(VERSION));
         difficulty = getIntent().getIntExtra("difficulty", 0);
+
+        switch (difficulty) {
+            case 1:
+                hintCount = 10;
+                tvHintCount.setText(Integer.toString(hintCount));
+                break;
+            case 2:
+                hintCount = 5;
+                tvHintCount.setText(Integer.toString(hintCount));
+                break;
+            case 3:
+                hintCount = 3;
+                tvHintCount.setText(Integer.toString(hintCount));
+                break;
+            case 4:
+                hintCount = 0;
+                tvHintCount.setText("ПОДСКАЗКИ НЕ ДОСТУПНЫ");
+                break;
+        }
+
         initMathActions();
         initButtonActions();
     }
@@ -68,13 +104,13 @@ public class MainActivity extends AppCompatActivity {
         tvFirstMathNumber = findViewById(R.id.tvFirstNumber);
         tvSecondMathNumber = findViewById(R.id.tvSecondNumber);
         tvMathSign = findViewById(R.id.tvMathSign);
-        //tvMotivation = findViewById(R.id.tvMotivation);
         tvAnswer = findViewById(R.id.tvAnswer);
+        tvVersion = findViewById(R.id.tvVersion);
+        tvHintCount = findViewById(R.id.tvHintCount);
 
         mButtonOk = findViewById(R.id.buttonOK);
         mButtonHelp = findViewById(R.id.buttonHelp);
         mButtonDelete = findViewById(R.id.buttonDelete);
-        //mButtonMore = findViewById(R.id.buttonMore);
         mEditTextAnswer = findViewById(R.id.editText_Answer);
         btn[0] = findViewById(R.id.btn0);
         btn[1] = findViewById(R.id.btn1);
@@ -94,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (tvAnswer.getText().toString().equals("?")) {
-                    Toast.makeText(getApplicationContext(), "Введите ответ", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(v, "ВВЕДИТЕ ОТВЕТ", Snackbar.LENGTH_SHORT).show();
                 } else {
                     MathCalculation calculation = new MathCalculation(firstNumber, secondNumber, intMathSign);
                     tempAnswer = calculation.getAnswer();
@@ -116,6 +152,7 @@ public class MainActivity extends AppCompatActivity {
             onClick(btn[i]);
         }
 
+        //кнопка УДАЛИТЬ
         mButtonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -125,12 +162,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //кнопка ПОДСКАЗКА
         mButtonHelp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MathCalculation calculation = new MathCalculation(firstNumber, secondNumber, intMathSign);
-                tempAnswer = calculation.getAnswer();
-                Toast.makeText(getApplicationContext(),Integer.toString(tempAnswer),Toast.LENGTH_SHORT).show();
+                if (hintCount == 0) {
+                    tvHintCount.setText("ПОДСКАЗКИ НЕ ДОСТУПНЫ");
+                } else {
+                    MathCalculation calculation = new MathCalculation(firstNumber, secondNumber, intMathSign);
+                    tempAnswer = calculation.getAnswer();
+                    Snackbar.make(v, Integer.toString(tempAnswer), Snackbar.LENGTH_SHORT).show();
+                    hintCount = hintCount - 1;
+                    tvHintCount.setText(Integer.toString(hintCount));
+                }
             }
         });
     }
@@ -142,7 +186,9 @@ public class MainActivity extends AppCompatActivity {
                 int i = Integer.parseInt(button.getText().toString());
                 mEditTextAnswer.setText("Нажмите ОК");
 
-                if (reset) {
+                if (tvAnswer.getText().toString().equals("0")) {
+                    tvAnswer.setText(button.getText().toString());
+                } else if (reset) {
                     tvAnswer.setText("");
                     tvAnswer.setText(Integer.toString(i));
                     reset = false;
